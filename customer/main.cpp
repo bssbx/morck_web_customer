affd30be5e4ff77b055b409135eeb0a7edc2a0ee
bssbx <104614354+bssbx@users.noreply.github.com>
2022-05-13 15:10:38 +0800

    update

---
+++ customer/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <oatpp/network/Server.hpp>
+#include <spdlog/spdlog.h>
+#include <httplib.h>
+#include <fmt/core.h>
+#include <tidy/tidy.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
